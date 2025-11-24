16a597ce39a2055cd871f058cd42b1143c3ee6a5
JoeNik <154469502@qq.com>
2025-11-24 21:35:05 +0800

    修改配置文件

--- backend/main.py
+++ backend/main.py
@@ -208,8 +208,8 @@ if __name__ == "__main__":
     
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
-        port=8000,
+        host=settings.HOST,
+        port=settings.PORT,
         reload=settings.APP_DEBUG,
         log_level="info"
     )
